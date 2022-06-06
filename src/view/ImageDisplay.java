58098553696d663a6e9dd0b01bb78bbccd405672
noamgreenstein <106999318+noamgreenstein@users.noreply.github.com>
2022-06-06 18:37:31 -0400

    started view

---
+++ src/view/ImageDisplay.java
@@ -0,0 +1,15 @@
+package view;
+
+import model.Image;
+import model.Pixel;
+
+public class ImageDisplay implements ImageView{
+  @Override
+  public void printImage(Image image) {
+    for (int i = 0; i < image.imagePixels.length; i++) {
+      for (int j = 0; j < image.imagePixels[0].length; j++) {
+          //print image.imagePixels[i][j] somehow
+      }
+    }
+  }
+}
---
+++ src/view/ImageView.java
@@ -0,0 +1,7 @@
+package view;
+
+import model.Image;
+
+public interface ImageView {
+  public void printImage(Image image);
+}
