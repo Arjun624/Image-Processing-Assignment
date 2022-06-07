a161bfc6184da6ec936740cef33f5a93288ff304
noamgreenstein <106999318+noamgreenstein@users.noreply.github.com>
2022-06-07 16:00:19 -0400

    controller program update

--- src/controller/ImageProgram.java
+++ src/controller/ImageProgram.java
@@ -3,16 +3,17 @@ package controller;
 public class ImageProgram {
   public static void main(String[] args) {
     String instruction = args[0];
-    String fileName = args[1];
+    String fileName;
     int increment;
     ImageController c;
     try {
-      increment = Integer.parseInt(args[2]);
-      c = new ImageControllerImpl(instruction, fileName, increment);
+      increment = Integer.parseInt(args[1]);
+      fileName = args[2];
     } catch (NumberFormatException e){
       increment = 0;
-      c = new ImageControllerImpl(instruction, fileName, increment);
+      fileName = args[1];
     }
+    c = new ImageControllerImpl(instruction, fileName, increment);
     c.editImage();
   }
 
