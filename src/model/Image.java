00120153faeb1aced0ad7527dab6ca7a1a65647a
Arjun624 <46407484+Arjun624@users.noreply.github.com>
2022-06-07 10:53:15 -0400

    did adjust brightness

--- src/model/Image.java
+++ src/model/Image.java
@@ -11,90 +11,121 @@ public class Image implements ImageEditor{
   @Override
   public Image flipVertically() {
 
-    Image newImage = this;
-    Pixel[][] pixels = newImage.imagePixels;
+    Pixel[][] newImagePixels = new Pixel[this.imagePixels.length][this.imagePixels[0].length];
     int yLength = imagePixels.length-1;
 
+    //System.arraycopy(pixels[yLength - row], 0, pixels[row], 0, imagePixels[0].length);
     for(int row = 0; row< imagePixels.length; row++){
       for(int col = 0; col<imagePixels[0].length; col++){
-        pixels[row][col]=pixels[yLength-row][col];
+        newImagePixels[row][col]=imagePixels[yLength-row][col];
       }
     }
 
-    return newImage;
+    return new Image(newImagePixels);
   }
 
   @Override
   public Image flipHorizontally() {
 
-    Image newImage = this;
-    Pixel[][] pixels = newImage.imagePixels;
+    Pixel[][] newImagePixels = new Pixel[this.imagePixels.length][this.imagePixels[0].length];
     int xLength = imagePixels[0].length-1;
 
     for(int row = 0; row< imagePixels.length; row++){
       for(int col = 0; col<imagePixels[0].length; col++){
-        pixels[row][col]=pixels[row][xLength-col];
+        newImagePixels[row][col]=imagePixels[row][xLength-col];
       }
     }
 
-    return newImage;
+    return new Image(newImagePixels);
   }
 
   @Override
   public Image showR() {
-    Pixel[][] newImage = new Pixel[this.imagePixels.length][this.imagePixels[0].length];
+    Pixel[][] newImagePixels = new Pixel[this.imagePixels.length][this.imagePixels[0].length];
     for (int row = 0; row < this.imagePixels.length; row++) {
       for (int col = 0; col < this.imagePixels[0].length; col++) {
         int red = this.imagePixels[row][col].r;
-        newImage[row][col]  = new Pixel(col,row,red,red,red);
+        newImagePixels[row][col]  = new Pixel(col,row,red,red,red);
       }
     }
-    return new Image(newImage);
+    return new Image(newImagePixels);
   }
 
   @Override
   public Image showG() {
-    Pixel[][] newImage = new Pixel[this.imagePixels.length][this.imagePixels[0].length];
+    Pixel[][] newImagePixels = new Pixel[this.imagePixels.length][this.imagePixels[0].length];
     for (int row = 0; row < this.imagePixels.length; row++) {
       for (int col = 0; col < this.imagePixels[0].length; col++) {
         int green = this.imagePixels[row][col].g;
-        newImage[row][col]  = new Pixel(col,row,green,green,green);
+        newImagePixels[row][col]  = new Pixel(col,row,green,green,green);
       }
     }
-    return new Image(newImage);
+    return new Image(newImagePixels);
   }
 
   @Override
   public Image showB() {
-    Pixel[][] newImage = new Pixel[this.imagePixels.length][this.imagePixels[0].length];
+    Pixel[][] newImagePixels = new Pixel[this.imagePixels.length][this.imagePixels[0].length];
     for (int row = 0; row < this.imagePixels.length; row++) {
       for (int col = 0; col < this.imagePixels[0].length; col++) {
         int blue = this.imagePixels[row][col].b;
-        newImage[row][col]  = new Pixel(col,row,blue,blue,blue);
+        newImagePixels[row][col]  = new Pixel(col,row,blue,blue,blue);
       }
     }
-    return new Image(newImage);
+    return new Image(newImagePixels);
   }
 
   @Override
   public Image adjustBrightness(int increment) {
-    return null;
+    Pixel[][] newImagePixels = new Pixel[this.imagePixels.length][this.imagePixels[0].length];
+    for(int row = 0; row< imagePixels.length; row++) {
+      for (int col = 0; col < imagePixels[0].length; col++) {
+        int newRed =  this.imagePixels[row][col].r + increment;
+        int newGreen = this.imagePixels[row][col].g + increment;
+        int newBlue = this.imagePixels[row][col].b + increment;
+        if (newRed > 255) {
+          newRed = 255;
+        }
+        if (newGreen > 255) {
+          newGreen = 255;
+        }
+        if (newBlue > 255) {
+          newBlue = 255;
+        }
+        if (newRed < 0) {
+          newRed = 0;
+        }
+        if (newGreen < 0) {
+          newGreen = 0;
+        }
+        if (newBlue < 0) {
+          newBlue = 0;
+        }
+        newImagePixels[row][col] = new Pixel(col, row, newRed, newGreen, newBlue);
+      }
+    }
+    return new Image(newImagePixels);
   }
 
   @Override
   public Image showLuma() {
+    //0.2126*R + 0.7152*G + 0.0722*B
     return null;
   }
 
   @Override
   public Image showIntensity() {
+    //average of r,g,b
     return null;
   }
 
   @Override
   public Image showValue() {
+    //max of r,g,b
     return null;
   }
+
+  //Grey scale methods?
 
 
 }
