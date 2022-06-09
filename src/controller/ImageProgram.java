c70786683ea3658ec98658e59bf20f2c85b565e2
noamgreenstein <106999318+noamgreenstein@users.noreply.github.com>
2022-06-08 20:04:19 -0400

    main

--- src/controller/ImageProgram.java
+++ src/controller/ImageProgram.java
@@ -2,11 +2,32 @@ package controller;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class ImageProgram {
   public static void main(String[] args) throws FileNotFoundException {
-    ImageControllerImpl controller = new ImageControllerImpl(args);
-    controller.go();
+    if(args.length < 3){
+      System.out.println("Enter all commands on one line");
+      Scanner s = new Scanner(System.in);
+      String info = s.nextLine();
+      Scanner scan = new Scanner(info);
+      List<String> input = new ArrayList<>();
+      while (scan.hasNext()){
+        input.add(scan.next());
+      }
+      String[] arr = input.toArray(new String[input.size()]);
+      ImageControllerImpl controller = new ImageControllerImpl(arr);
+      controller.go();
+    }
+    else {
+      ImageControllerImpl controller = new ImageControllerImpl(args);
+      controller.go();
+    }
+
   }
 
 }
