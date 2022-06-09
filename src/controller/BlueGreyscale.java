f52f365452002042763f2e9b5bdd37cd0cd1c75d
noamgreenstein <106999318+noamgreenstein@users.noreply.github.com>
2022-06-09 12:09:48 -0400

    fixed blue

--- src/controller/BlueGreyscale.java
+++ src/controller/BlueGreyscale.java
@@ -8,17 +8,14 @@ import view.ImageView;
 public class BlueGreyscale implements ImageCommands {
   String fileName;
   String newFileName;
-  ImageView view;
 
-  public BlueGreyscale(String fileName, String newFileName, ImageView view) {
+  public BlueGreyscale(String fileName, String newFileName) {
     this.fileName = fileName;
     this.newFileName = newFileName;
-    this.view = view;
   }
 
   @Override
   public void execute(ImageModel model) throws IOException {
     model.blueGreyscale(fileName, newFileName);
-    view.renderMessage("Created blue greyscale of " + fileName);
   }
 }
