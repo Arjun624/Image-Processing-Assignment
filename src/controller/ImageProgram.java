90edf943bc2261f6fe06d3e3dcc80df0a7bf95cb
noamgreenstein <106999318+noamgreenstein@users.noreply.github.com>
2022-06-22 17:50:01 -0400

    program

--- src/controller/ImageProgram.java
+++ src/controller/ImageProgram.java
@@ -1,8 +1,12 @@
 package controller;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import model.ImageModel;
+import view.ImageDisplay;
+import view.ImageView;
 
 /**
  * The class that runs the program in its entirety.
@@ -15,35 +19,34 @@ public class ImageProgram {
    * @throws IOException if the program cannot read the input or write the output
    */
   public static void main(String[] args) throws IOException {
-//    ImageControllerImpl controller;
-//    ImageView view = new ImageDisplay(System.out);
-//    ImageModel model = new ImageModel();
-//    if (args.length > 0) {
-//      for (int i = 0; i < args.length - 1; i++) {
-//        if (args[i].contains("-file")) {
-//          try {
-//            InputStreamReader in = new FileReader(args[i + 1]);
-//            controller = new ImageControllerImpl(view, in, model);
-//            controller.start();
-//            return;
-//          } catch (IOException e) {
-//            view.renderMessage("Error: Cannot read file, please check arguments.");
-//            return;
-//          }
-//
-//        }
-//      }
-//    }
-//    InputStreamReader in = new InputStreamReader(System.in);
-//    view.displayWelcomeMessage();
-//    controller = new ImageControllerImpl(view, in, model);
-//    controller.start();
-//
-
     ImageModel model = new ImageModel();
+    ImageControllerImpl controller;
+    ImageView view = new ImageDisplay(System.out);
+    if (args.length > 0) {
+        if (args[0].equalsIgnoreCase("-file")) {
+          try {
+            InputStreamReader in = new FileReader(args[1]);
+            controller = new ImageControllerImpl(view, in, model);
+            controller.start();
+            return;
+          } catch (IOException e) {
+            view.renderMessage("Error: Cannot read file, please check arguments.");
+            return;
+          }
+          catch (NullPointerException e) {
+            view.renderMessage("Error: No file inputted!");
+            return;
+          }
+        } else if (args[0].equalsIgnoreCase("-text")) {
+          InputStreamReader in = new InputStreamReader(System.in);
+          view.displayWelcomeMessage();
+          controller = new ImageControllerImpl(view, in, model);
+          controller.start();
+          return;
+        }
 
-
-    ImageControllerGUI controller = new ImageControllerGUI(model);
+    }
+    ImageControllerGUI controllerGUI = new ImageControllerGUI(model);
 
   }
 }
